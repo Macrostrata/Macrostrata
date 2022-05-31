1c749794984d9892c682fc557b49b1af230af5ac
Prince Merluza <zero.bash@gmail.com>
2022-05-31 16:35:44 +0800

    parse json

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -2,7 +2,8 @@ const GITHUB_ORG = 'Macrostrata';
 const BP_INDEX_URL = `https://${GITHUB_ORG}.github.io/${GITHUB_ORG}/blueprints/index.json`;
 
 fetch(BP_INDEX_URL, { method: 'GET' })
-.then((response) => {
-    console.log(response);
+.then(response => response.json())
+.then(data => {
+    console.log(data);
 });
 

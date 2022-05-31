e85b990905eca7df4b443a969c0b67efcdc8ec3b
Prince Merluza <zero.bash@gmail.com>
2022-05-31 16:46:36 +0800

    Get blueprint results

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -1,9 +1,25 @@
 const GITHUB_ORG = 'Macrostrata';
-const BP_INDEX_URL = `https://${GITHUB_ORG}.github.io/${GITHUB_ORG}/blueprints/index.json`;
+const BLUEPRINTS_INDEX_URL = `https://${GITHUB_ORG}.github.io/${GITHUB_ORG}/blueprints/index.json`;
+const BLUEPRINTS_URL = `https://${GITHUB_ORG}.github.io/${GITHUB_ORG}/blueprints/`;
 
-fetch(BP_INDEX_URL, { method: 'GET' })
-.then(response => response.json())
-.then(data => {
-    console.log(data);
-});
 
+function getBlueprints() {
+  // Get the index of blueprints
+  return fetch(BLUEPRINTS_INDEX_URL)
+    .then(response => response.json())
+    .then(data => {
+      const blueprintPromises = [];
+
+      data.forEach(blueprint => {
+        const blueprintURL = `${BLUEPRINTS_URL}${blueprint}.json`;
+        blueprintPromises.push(fetch(blueprintURL).then(response => response.json()));
+        console.log(`Getting ${blueprint} result`);
+      });
+
+      return Promise.all();
+    })
+    .then(resultsArr => {
+      console.log('Got the blueprint results.')
+      console.log(resultsArr);
+    });
+}
